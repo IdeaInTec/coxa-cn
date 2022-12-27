b8722a9a001ff8c233a462f2af854b8f1760e5ff
Monirul Islam <65311288+monirulislamweb@users.noreply.github.com>
2022-12-27 13:41:47 +0600

    ..

--- themes/coxa/templates/page-banner.php
+++ themes/coxa/templates/page-banner.php
@@ -1,10 +1,11 @@
 <?php 
 $thisID = get_the_ID();
 $banner = get_field('banner', $thisID);
-$desktop_banner = !empty($banner['afbeelding'])?cbv_get_image_src($banner['afbeelding']):banner_placeholder();
-$mobiel_banner = !empty($banner['mobiel_afbeelding'])?cbv_get_image_src($banner['mobiel_afbeelding']):banner_placeholder();
-$custom_titel = get_field('custom_page_titel', $thisID);
+$banner_bg = !empty($banner['image'])?cbv_get_image_src($banner['image']):banner_placeholder();
+$custom_titel = $banner['title'];
 $page_titel = !empty($custom_titel)?$custom_titel:get_the_title($thisID);
+$link_1 = $banner['link_1'];
+$link_2 = $banner['link_2'];
 
 ?>
 
@@ -48,3 +49,49 @@ $page_titel = !empty($custom_titel)?$custom_titel:get_the_title($thisID);
     </div>
   </div>
 </section>
+
+
+<section class="page-banner">
+  <div class="banner-black-bg"></div>
+  <div class="page-bnr-bg parallaxie" style="background-image: url(assets/images/about-pg-banner-bg.jpg);"></div>
+  <div class="bnr-vdo d-none">
+    <video id="bt-vdo" autoplay="true" muted="" loop>
+      <source src="assets/images/videos/placeholder-video.mp4" type="video/mp4">
+      </video>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="pg-banner-cntlr">
+            <div class="pg-banner-desc-cntlr">
+              <h1 class="pg-banner-title fl-h3">About Us</h1>
+              <div class="diamond-module">
+                <ul class="reset-list">
+                  <li>
+                    <i><img src="assets/images/sec-title-diamond.svg" alt=""></i>
+                  </li>
+                  <li>
+                    <i><img src="assets/images/sec-title-diamond.svg" alt=""></i>
+                  </li>
+                  <li>
+                    <i><img src="assets/images/sec-title-diamond.svg" alt=""></i>
+                  </li>
+                </ul>
+              </div> 
+              <div class="pg-banner-desc">
+                <p>Experts in Dentistry for over 30 years</p>
+              </div>
+              <div class="pg-banner-btns">
+                <div class="pg-banner-btn pg-banner-btn-01">
+                  <a class="cdc-btn" href="#">Contact Us</a>
+                </div>
+                <div class="pg-banner-btn pg-banner-btn-02">
+                  <a class="cdc-trnsprnt-btn" href="#">Our Practice</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
