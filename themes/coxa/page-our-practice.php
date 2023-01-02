585b384b75246838abe72186d8839d2505b4a0d5
Shariful Islam <56506215+Shariful700453@users.noreply.github.com>
2023-01-02 19:06:34 +0600

    ,,,

--- themes/coxa/page-our-practice.php
+++ themes/coxa/page-our-practice.php
@@ -599,6 +599,38 @@ $link = $team['link'];
 							</div>
 							<?php endwhile; ?>
 						</div>
+						<div class="team-prev-next">
+		                  <div class="revw-prev">
+		                    <span class="revw-left">
+		                      <svg class="revw-left-arrow" width="11" height="34" viewBox="0 0 11 34" fill="#29467D">
+		                        <use xlink:href="#revw-left-arrow"></use> 
+		                      </svg>
+		                    </span>
+		                  </div>
+		                  <div class="revw-next">
+		                    <span class="revw-right">
+		                      <svg class="revw-right-arrow" width="11" height="34" viewBox="0 0 11 34" fill="#29467D">
+		                        <use xlink:href="#revw-right-arrow"></use> 
+		                      </svg>
+		                    </span>
+		                  </div>
+		                </div>
+		                <div class="xs-team-prev-next">
+		                  <div class="revw-prev">
+		                    <span class="revw-left">
+		                      <svg class="prev-line" width="66" height="20" viewBox="0 0 66 20" fill="#29467D">
+		                        <use xlink:href="#prev-line"></use> 
+		                      </svg>
+		                    </span>
+		                  </div>
+		                  <div class="revw-next">
+		                    <span class="revw-right">
+		                      <svg class="next-line" width="66" height="20" viewBox="0 0 66 20" fill="#29467D">
+		                        <use xlink:href="#next-line"></use> 
+		                      </svg>
+		                    </span>
+		                  </div>
+		                </div> 
 						<?php if(is_array($link) && !empty($link['url'])): ?>
 						<div class="wco-team-btn">
 							<?php printf('<a class="cdc-btn" href="%s">%s</a>',$link['url'],$link['title'] ); ?>
