ba2e147a6eaa112ccf55d9ab9d03e52e47bd5df0
Monirul Islam <65311288+monirulislamweb@users.noreply.github.com>
2023-01-04 21:05:22 +0600

    ..

--- themes/coxa/page.php
+++ themes/coxa/page.php
@@ -1,11 +1,43 @@
 <?php 
 	get_header(); 
-	get_template_part('templates/page', 'banner');
+  while( have_posts()): the_post();
+  $thisID = get_the_ID();
+	$banner = get_field('banner', $thisID);
+$banner_bg = !empty($banner['image'])?cbv_get_image_src($banner['image']):banner_placeholder();
+$page_titel = !empty($banner['title'])?$banner['title']:get_the_title($thisID);
 ?>
 
-<?php
-	$description = get_field('description');
-?>
+<section class="page-banner <?php echo (!empty($banner['description'])? '': 'diamond-module-none'); ?>">
+  <div class="banner-black-bg"></div>
+      <?php if( !empty($banner_bg) ): ?>
+      <div class="page-bnr-bg parallaxie" style="background-image: url('<?php echo $banner_bg; ?>')"></div>
+      <?php endif;?>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="pg-banner-cntlr">
+            <div class="pg-banner-desc-cntlr">
+              <h1 class="pg-banner-title fl-h3"><?php echo ($page_titel); ?></h1>
+              <div class="diamond-module">
+                <ul class="reset-list">
+                  <li>
+                    <i><img src="<?php echo THEME_URI; ?>/assets/images/sec-title-diamond.svg" alt=""></i>
+                  </li>
+                  <li>
+                    <i><img src="<?php echo THEME_URI; ?>/assets/images/sec-title-diamond.svg" alt=""></i>
+                  </li>
+                  <li>
+                    <i><img src="<?php echo THEME_URI; ?>/assets/images/sec-title-diamond.svg" alt=""></i>
+                  </li>
+                </ul>
+              </div> 
+              <?php if(!empty($banner['description'])) echo ('<div class="pg-banner-desc">'.wpautop($banner['description']).'</div>'); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
 
 <div class="default-page-con-cntlr">
   <section class="privacy-policy-sec">
@@ -15,7 +47,7 @@
           <div class="privacy-plcy-sec-inr">
             <div class="privacy-plcy-sec-cntlr">
               <div class="privacy-plcy-des">
-                <?php if(!empty($description)) echo $description; ?>
+                <?php the_content(); ?>
               </div>
             </div>
           </div>
@@ -26,4 +58,4 @@
 </div>
 
 
-<?php get_footer(); ?>
+<?php endwhile; get_footer(); ?>
